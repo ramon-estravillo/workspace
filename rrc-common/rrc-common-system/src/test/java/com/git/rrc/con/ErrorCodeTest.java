cebf8a3df77b16db29ca107dfb3c5edcd1f1a89f
ramon.estravillo <ramonestravillo@gmail.com>
2025-07-15 21:02:55 +0800

    Add ErrorCodeTest

--- rrc-common/rrc-common-system/src/test/java/com/git/rrc/con/ErrorCodeTest.java
+++ rrc-common/rrc-common-system/src/test/java/com/git/rrc/con/ErrorCodeTest.java
@@ -1,14 +1,50 @@
 package com.git.rrc.con;
 
-import org.junit.Test;
+import com.git.rrc.abs.ReasonPhrase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockStatic;
 
-public class ErrorCodeTest {
+class ErrorCodeTest {
 
     @Test
-    public void test1() {
-        assertNotNull(ErrorCode.valueOf(3100));
+    @DisplayName("Should return corresponding name of the ErrorCode")
+    void getErrorCode_shouldReturnConstantName_whenValueIsPresent() {
+        ErrorCode code = ErrorCode.valueOf(2000);
+        assertEquals("BAD_REQUEST", code.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("T")
+    void getReasonPhrase_shouldErrorCodeDetailedMessage_whenValueIsPresent() {
+        try(MockedStatic<ReasonPhrase> reasonPhraseMockedStatic = mockStatic(ReasonPhrase.class)) {
+            reasonPhraseMockedStatic.when(() -> ReasonPhrase.valueOf(2000)).thenReturn("Bad Request");
+
+            String actual = ErrorCode.BAD_REQUEST.getReasonPhrase();
+            assertEquals("Bad Request", actual);
+            reasonPhraseMockedStatic.verify(() -> ReasonPhrase.valueOf(2000));
+        }
+    }
+
+    @Test
+    @DisplayName("Should return corresponding ErrorCode")
+    void valueOf_shouldReturnErrorCode_whenValueIsPresent() {
+        assertNotNull(ErrorCode.valueOf(2400));
+    }
+
+    @Test
+    @DisplayName("Should throw an EnumConstantNotPresentException")
+    void valueOf_shouldThrowEnumConstantNotPresentException_whenValueIsNotPresent() {
+        try {
+            ErrorCode.valueOf(1000);
+        } catch (Exception e) {
+            if(!(e instanceof EnumConstantNotPresentException)) {
+                fail("EnumConstantNotPresentException expected but instead got " + e);
+            }
+        }
     }
 
 }
